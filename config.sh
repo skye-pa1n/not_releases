da613468f5ea241139e6def5c05cf309ac76b88a
@skye // pa1n <sonicgames772@gmail.com>
2025-02-22 19:30:18 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="skye-pa1n"
+export GITHUB_EMAIL="sonicgames772@gmail.com"
 
-export device=""
+export device="r8q"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Not Kernel"
+export ROM_DIR="not_r8q"
+export ROM_VERSION="interstellar"
 export local_manifest_url=""
 export manifest_url=""
 export rom_vendor_name=""
 export branch=""
-export bacon="bacon"
+export bacon="sh skye.sh"
 export buildtype=""
 export clean=""
 export generate_incremental=""
